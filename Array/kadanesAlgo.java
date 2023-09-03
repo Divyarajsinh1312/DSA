cce570855b38a8a69c71a27da05cf65623a4d30e
Divyrajsinh Sindhav <divyrajsinh@Divyrajsinhs-MacBook-Air.local>
2023-09-03 11:44:15 +0530

    3rd Sep 2023

---
+++ Array/kadanesAlgo.java
@@ -0,0 +1,26 @@
+package Array;
+
+/**
+ * kadanesAlgo
+ */
+public class kadanesAlgo {
+    public static int kadanes(int arr[]){
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if (sum<0) {
+                sum = 0;
+            }
+            if (sum>max) {
+                max = sum;
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int array[] = {-2,-3,4,-1,-2,1,5,-3};
+        int max = kadanes(array);
+        System.out.println("Max of Sub Array Sum is " + max);
+    }
+}
