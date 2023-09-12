53d04d8458efb31ccfd27c46db1d3a20dd3535c3
Divyrajsinh Sindhav <divyrajsinh3315@gmail.com>
2023-09-13 01:52:17 +0530

    13th Sep 2023

---
+++ Searching/smallestLetter.java
@@ -0,0 +1,29 @@
+package Searching;
+/**
+ * smallestLetter
+ */
+public class smallestLetter {
+    public static char smallest(char letters[], char target){
+        int start = 0;
+        int end = letters.length - 1;
+
+        while(start <= end) {
+            // find the middle element
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+            int mid = start + (end - start) / 2;
+
+            if (target < letters[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return letters[start % letters.length];
+    }
+    public static void main(String[] args) {
+        char letter[] = {'c', 'f', 'j'};
+        char target = 'z';
+        char smallestLetter = smallest(letter, target);
+        System.out.println("Answer is " + smallestLetter);
+    }
+}
