130da2af41123376493f324fac545a4a3637dd89
Divyarajsinh Sindhav <106440736+divyarajsinhsindhav@users.noreply.github.com>
2024-01-28 16:09:21 +0530

    Tree

---
+++ Tree/BST.java
@@ -0,0 +1,121 @@
+package Tree;
+import java.util.Scanner;
+
+public class BST {
+
+    public class Node {
+        private int value;
+        private Node left;
+        private Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+    private Node root;
+
+    public BST() {
+
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public void insert(int value) {
+        root = insert(value, root);
+    }
+
+    private Node insert(int value, Node node) {
+        if (node == null) {
+            return new Node(value);
+        }
+
+        if (value < node.value) {
+            node.left = insert(value, node.left);
+        } else if (value > node.value) {
+            node.right = insert(value, node.right);
+        }
+
+        return node;
+    }
+
+    public void populate(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            this.insert(nums[i]);
+        }
+    }
+
+    public void display() {
+        display(root, "Root Node: ");
+    }
+
+    private void display(Node node, String details) {
+        if (node == null) {
+            return;
+        }
+
+        System.out.println(details + node.value);
+        display(node.left, "Left child of " + node.value + " : ");
+        display(node.right, "Right child of " + node.value + " : ");
+    }
+
+    //BST Traversal
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    private void preOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        System.out.print(node.value + " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    public void postOrder() {
+        postOrder(root);
+    }
+
+    private void postOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.value + " ");
+    }
+
+    public void inOrder() {
+        inOrder(root);
+    }
+
+    private void inOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left);
+        System.out.print(node.value + " ");
+        inOrder(node.right);
+    }
+
+    public static void main(String[] args) {
+        BST tree = new BST();
+        int[] nums = { 5, 2, 7, 1, 4, 6, 9, 8, 3, 10 };
+        tree.populate(nums);
+        tree.display();
+        System.out.println("\n\nBST Traversal:");
+        System.out.println("\nPreorder:");
+        tree.preOrder();
+        System.out.println("\nInOrder:");
+        tree.inOrder();
+        System.out.println("\nPostorder:");
+        tree.postOrder();
+    }
+}
